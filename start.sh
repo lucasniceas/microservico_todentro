ab604d158450d00dc671a9a64d314f2db34c0b6b
Lucas Nicéas <lucassniceaspt@hotmail.com>
2025-01-29 19:21:48 +0000

    Corrigindo instalação do FFmpeg

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "🔹 Atualizando pacotes e instalando FFmpeg..."
 apt-get update && apt-get install -y ffmpeg
+
+echo "✅ FFmpeg instalado com sucesso. Iniciando aplicação..."
 python3 main.py
